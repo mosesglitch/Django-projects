83b6281704ca6ad31e4b6e520ec6bfa083440fc8
mosesglitch <82958035+mosesglitch@users.noreply.github.com>
2022-07-21 11:05:02 +0300

    Delete passproject directory

--- passproject/passap/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from passap.models import UserProfileInfo
-# Register your models here.
-admin.site.register(UserProfileInfo)
-
--- passproject/passap/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from passap import views
-
-app_name='passap'
-
-urlpatterns = [
-    path('register/',views.register,name='register'),
-    path('userlogin/',views.user_login,name='user_login'),
-]
